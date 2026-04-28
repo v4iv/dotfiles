e1cbc598395e6884cbbf22221edbba5f0b3d1e23
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2026-04-28 13:36:35 +0530

    replace lazygit and alpha with snacks

--- .config/nvim/lua/waybove/plugins/snacks.lua
+++ .config/nvim/lua/waybove/plugins/snacks.lua
@@ -10,20 +10,127 @@ return {
 			-- refer to the configuration section below
 			bigfile = { enabled = true },
 			bufdelete = { enabled = true },
-			indent = {
+			dashboard = {
 				enabled = true,
-				hl = {
-					"SnacksIndent1",
-					"SnacksIndent2",
-					"SnacksIndent3",
-					"SnacksIndent4",
-					"SnacksIndent5",
-					"SnacksIndent6",
-					"SnacksIndent7",
-					"SnacksIndent8",
+				preset = {
+					-- Used by the `keys` section to show keymaps.
+					-- Set your custom keymaps here.
+					-- When using a function, the `items` argument are the default keymaps.
+					---@type snacks.dashboard.Item[]
+					keys = {
+						{
+							icon = " ",
+							key = "f",
+							desc = "Find File",
+							action = function()
+								require("telescope.builtin").find_files({
+									hidden = true,
+									find_command = {
+										"rg",
+										"--files",
+										"--hidden",
+										"--glob",
+										"!**/.git/*",
+									},
+								})
+							end,
+						},
+						{ icon = " ", key = "e", desc = "File Explorer", action = "<cmd>NvimTreeToggle<CR>" },
+						{
+							icon = "",
+							key = "g",
+							desc = "Find Text",
+							action = ":lua Snacks.dashboard.pick('live_grep')",
+						},
+						{
+							icon = " ",
+							key = "r",
+							desc = "Recent Files",
+							action = ":lua Snacks.dashboard.pick('oldfiles')",
+						},
+						{
+							icon = " ",
+							key = "c",
+							desc = "Config",
+							action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+						},
+						{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+						{
+							icon = "󰒲 ",
+							key = "L",
+							desc = "Lazy",
+							action = ":Lazy",
+							enabled = package.loaded.lazy ~= nil,
+						},
+						{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+					},
+					-- Used by the `header` section
+					header = [[
+			 ██████╗  ██████╗ ███╗   ██╗██╗████████╗
+			 ██╔══██╗██╔═══██╗████╗  ██║╚═╝╚══██╔══╝
+			 ██║  ██║██║   ██║██╔██╗ ██║      ██║   
+			 ██║  ██║██║   ██║██║╚██╗██║      ██║   
+			 ██████╔╝╚██████╔╝██║ ╚████║      ██║   
+			 ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝      ╚═╝   
+			                                        
+			 ██████╗  █████╗ ███╗   ██╗██╗ ██████╗  
+			 ██╔══██╗██╔══██╗████╗  ██║██║██╔════╝  
+			 ██████╔╝███████║██╔██╗ ██║██║██║       
+			 ██╔═══╝ ██╔══██║██║╚██╗██║██║██║       
+			 ██║     ██║  ██║██║ ╚████║██║╚██████╗  
+			 ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝ ╚═════╝  
+          ]],
+				},
+				sections = {
+					{ section = "header" },
+					{
+						section = "terminal",
+						cmd = "krabby random --no-title; sleep .1",
+						random = 10,
+						pane = 2,
+						indent = 4,
+						height = 20,
+					},
+					{ section = "keys", gap = 1, padding = 1 },
+					-- {
+					-- 	pane = 2,
+					-- 	icon = " ",
+					-- 	title = "Recent Files",
+					-- 	section = "recent_files",
+					-- 	indent = 2,
+					-- 	padding = 1,
+					-- },
+					{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+					{
+						pane = 2,
+						icon = " ",
+						title = "Git Status",
+						section = "terminal",
+						enabled = function()
+							return Snacks.git.get_root() ~= nil
+						end,
+						cmd = "git status --short --branch --renames",
+						height = 5,
+						padding = 1,
+						ttl = 5 * 60,
+						indent = 3,
+					},
+					{ section = "startup" },
 				},
 			},
+			git = { enabled = true },
+			gitbrowse = { enabled = true },
+			indent = {
+				enabled = true,
+				hl = "SnacksIndent",
+			},
 			input = { enabled = true },
+			lazygit = {
+				enabled = true,
+				-- automatically configure lazygit to use the current colorscheme
+				-- and integrate edit with the current neovim instance
+				configure = true,
+			},
 			picker = { enabled = true },
 			notifier = { enabled = true },
 			quickfile = { enabled = true },
@@ -256,6 +363,21 @@ return {
 					Snacks.notifier.hide()
 				end,
 				desc = "Dismiss All Notifications",
+			},
+			{
+				"<leader>gB",
+				function()
+					Snacks.gitbrowse()
+				end,
+				desc = "Git Browse",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>lg",
+				function()
+					Snacks.lazygit()
+				end,
+				desc = "Lazygit",
 			},
 			{
 				"<leader>h",
