baceba997d97032ff7c06192e5330a0c9c131a49
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2026-05-05 16:32:32 +0530

    install chafa plugin for nvim

--- .config/nvim/lua/waybove/plugins/init.lua
+++ .config/nvim/lua/waybove/plugins/init.lua
@@ -2,4 +2,5 @@ return {
 	{ "nvim-lua/plenary.nvim" }, -- lua functions that many plugins use
 	{ "nvim-mini/mini.nvim", version = false },
 	{ "nvim-tree/nvim-web-devicons", opts = {} },
+	{ "m00qek/baleia.nvim" },
 }
