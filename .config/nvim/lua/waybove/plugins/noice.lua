590cda50cc2918a082b04ef66e6fc699e0ee1acd
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2026-05-25 19:45:16 +0530

    configure noice popup position

--- .config/nvim/lua/waybove/plugins/noice.lua
+++ .config/nvim/lua/waybove/plugins/noice.lua
@@ -12,7 +12,41 @@ return {
 			-- OPTIONAL:
 			--   `nvim-notify` is only needed, if you want to use the notification view.
 			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
+			-- "rcarriga/nvim-notify",
 		},
+		config = function()
+			require("noice").setup({
+				views = {
+					cmdline_popup = {
+						position = {
+							row = 5,
+							col = "50%",
+						},
+						size = {
+							width = 60,
+							height = "auto",
+						},
+					},
+					popupmenu = {
+						relative = "editor",
+						position = {
+							row = 8,
+							col = "50%",
+						},
+						size = {
+							width = 60,
+							height = 10,
+						},
+						border = {
+							style = "rounded",
+							padding = { 0, 1 },
+						},
+						win_options = {
+							winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+						},
+					},
+				},
+			})
+		end,
 	},
 }
