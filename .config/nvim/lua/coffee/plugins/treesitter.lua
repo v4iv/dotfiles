97f54d475e8f9b4933ab72b0af73436f15f6ef18
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2026-08-11 23:38:50 +0530

    update helix configs
    added yazi as explorer and lazgit for git support

--- .config/nvim/lua/coffee/plugins/treesitter.lua
+++ .config/nvim/lua/coffee/plugins/treesitter.lua
@@ -25,11 +25,23 @@ return {
 					end
 				end,
 			},
+			folds = { enable = true },
 			ensure_installed = "all",
 		},
 
 		config = function(_, opts)
 			require("nvim-treesitter").setup(opts)
+
+			-- Automatically start treesitter for supported filetypes
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local lang = vim.treesitter.language.get_lang(args.match) or args.match
+					local installed = require("nvim-treesitter").get_installed("parsers")
+					if vim.tbl_contains(installed, lang) then
+						vim.treesitter.start(args.buf)
+					end
+				end,
+			})
 
 			-- use bash parser for zsh files
 			vim.treesitter.language.register("bash", "zsh")
