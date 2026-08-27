aff3bcdceeb029077b9a0bba6cd82cedeccf4099
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2026-08-27 11:18:05 +0530

    update debugger plugin for nvim

--- .config/nvim/lua/coffee/plugins/debugger.lua
+++ .config/nvim/lua/coffee/plugins/debugger.lua
@@ -5,41 +5,112 @@ return {
 		dependencies = {
 			{ "rcarriga/nvim-dap-ui", dependencies = { "nvim-neotest/nvim-nio" } },
 			"theHamsta/nvim-dap-virtual-text",
+			"mxsdev/nvim-dap-vscode-js",
+			-- build debugger from source
+			{
+				"microsoft/vscode-js-debug",
+			},
+		},
+		keys = {
+			-- normal mode is default
+			{
+				"<leader>dt",
+				function()
+					require("dap").toggle_breakpoint()
+				end,
+			},
+			{
+				"<leader>dc",
+				function()
+					require("dap").continue()
+				end,
+			},
+			{
+				"<leader>dso",
+				function()
+					require("dap").step_over()
+				end,
+			},
+			{
+				"<leader>dsi",
+				function()
+					require("dap").step_into()
+				end,
+			},
+			{
+				"<leader>dsu",
+				function()
+					require("dap").step_out()
+				end,
+			},
 		},
 		config = function()
-			local map = vim.keymap.set
-			local dap = require("dap")
-			local dapui = require("dapui")
-			local dapvirtualtext = require("nvim-dap-virtual-text")
+			require("dap-vscode-js").setup({
+				debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+				adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+			})
 
-			dapui.setup()
-			dapvirtualtext.setup({})
-
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
+			for _, language in ipairs({ "typescript", "javascript", "svelte" }) do
+				require("dap").configurations[language] = {
+					-- attach to a node process that has been started with
+					-- `--inspect` for longrunning tasks or `--inspect-brk` for short tasks
+					-- npm script -> `node --inspect-brk ./node_modules/.bin/vite dev`
+					{
+						-- use nvim-dap-vscode-js's pwa-node debug adapter
+						type = "pwa-node",
+						-- attach to an already running node process with --inspect flag
+						-- default port: 9222
+						request = "attach",
+						-- allows us to pick the process using a picker
+						processId = require("dap.utils").pick_process,
+						-- name of the debug action you have to select for this config
+						name = "Attach debugger to existing `node --inspect` process",
+						-- for compiled languages like TypeScript or Svelte.js
+						sourceMaps = true,
+						-- resolve source maps in nested locations while ignoring node_modules
+						resolveSourceMapLocations = {
+							"${workspaceFolder}/**",
+							"!**/node_modules/**",
+						},
+						-- path to src in vite based projects (and most other projects as well)
+						cwd = "${workspaceFolder}/src",
+						-- we don't want to debug code inside node_modules, so skip it!
+						skipFiles = { "${workspaceFolder}/node_modules/**/*.js" },
+					},
+					{
+						type = "pwa-chrome",
+						name = "Launch Chrome to debug client",
+						request = "launch",
+						url = "http://localhost:5173",
+						sourceMaps = true,
+						protocol = "inspector",
+						port = 9222,
+						webRoot = "${workspaceFolder}/src",
+						-- skip files from vite's hmr
+						skipFiles = { "**/node_modules/**/*", "**/@vite/*", "**/src/client/*", "**/src/*" },
+					},
+					-- only if language is javascript, offer this debug action
+					language == "javascript" and {
+						-- use nvim-dap-vscode-js's pwa-node debug adapter
+						type = "pwa-node",
+						-- launch a new process to attach the debugger to
+						request = "launch",
+						-- name of the debug action you have to select for this config
+						name = "Launch file in new node process",
+						-- launch current file
+						program = "${file}",
+						cwd = "${workspaceFolder}",
+					} or nil,
+				}
 			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
 
-			map("n", "<leader>dt", dap.toggle_breakpoint, { desc = "Toggle Break" })
-			map("n", "<leader>dc", dap.continue, { desc = "Continue" })
-			map("n", "<leader>dr", dap.repl.open, { desc = "Inspect" })
-			map("n", "<leader>dk", dap.terminate, { desc = "Kill" })
-
-			map("n", "<leader>dso", dap.step_over, { desc = "Step Over" })
-			map("n", "<leader>dsi", dap.step_into, { desc = "Step Into" })
-			map("n", "<leader>dsu", dap.step_out, { desc = "Step Out" })
-			map("n", "<leader>dl", dap.run_last, { desc = "Run Last" })
-
-			map("n", "<leader>duu", dapui.open, { desc = "Open DAP UI" })
-			map("n", "<leader>duc", dapui.close, { desc = "Close DAP UI" })
+			require("dapui").setup()
+			local dap, dapui = require("dap"), require("dapui")
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open({ reset = true })
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+			dap.listeners.before.event_exited["dapui_config"] = dapui.close
 		end,
 	},
 }
