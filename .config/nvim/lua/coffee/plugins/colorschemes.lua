5ea60c94fb4f193860a24c360cb0decde2cf0781
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2026-09-07 20:02:42 +0530

    customize houston colorscheme

--- .config/nvim/lua/coffee/plugins/colorschemes.lua
+++ .config/nvim/lua/coffee/plugins/colorschemes.lua
@@ -5,13 +5,22 @@ return {
 	opts = {
 		transparent = false,
 		italic_comments = true,
-		styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-			keyword = { italic = true },
-			conditionals = { italic = true },
-			types = { italic = true },
-			functions = {},
-			variables = {},
-			booleans = {},
-		},
+		on_colors = function(c)
+			c.bg = "#342A57"
+			c.bg_panel = "#3F3467" -- swapped to keep panel darker than float
+			c.bg_float = "#4F427D"
+		end,
+		on_highlights = function(hl, c)
+			-- Legacy & standard syntax groups
+			hl.Keyword = { italic = true }
+			hl.Conditional = { italic = true }
+			hl.Type = { italic = true }
+
+			-- Treesitter capture groups (@-prefixed)
+			hl["@keyword"] = { italic = true }
+			hl["@keyword.conditional"] = { italic = true }
+			hl["@type"] = { italic = true }
+			hl["@type.builtin"] = { italic = true }
+		end,
 	},
 }
